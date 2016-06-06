f000cab148e8e190a5cf605e5ce20bab47cf76c6
Neo <neolwc@gmail.com>
2016-06-06 00:11:57 -0400

    :rocket:

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,31 @@ var $ = require('gulp-load-plugins')();
 var autoprefixer = require('autoprefixer');
 var browserSync = require('browser-sync').create();
 
+gulp.task('build', ['css', 'html', 'images']);
+
+gulp.task('clean', function() {
+	return gulp.src('dist', {read: false})
+		.pipe($.clean());
+})
+
+gulp.task('css', ['styles'], function() {
+	return gulp.src('src/*.css')
+		.pipe($.cssmin())
+		.pipe(gulp.dest('dist'));
+})
+
+gulp.task('html', function() {
+	return gulp.src('src/*.html')
+		.pipe($.htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function() {
+	return gulp.src('src/images/*')
+		.pipe($.imagemin())
+		.pipe(gulp.dest('dist/images'));
+});
+
 gulp.task('styles', function() {
 	return gulp.src('src/styles/*.scss')
 		.pipe($.sass().on('error', $.sass.logError))
